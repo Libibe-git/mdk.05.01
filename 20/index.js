4eb85d44e7017b82728fa148da0e1dd4dd59af34
Havi <libebe@mail.ru>
2026-06-21 17:25:39 +0300

    Исправлен билет 20

--- 20/index.js
+++ 20/index.js
@@ -1,9 +1,11 @@
 let numb = document.getElementById('numb')
 
+let spisok = []
+
 let a = 1
 while (a<10) {
     a++
     console.log(a);
-    
+    spisok.push(a)
 }
-numb.textContent = a
+numb.textContent = spisok
